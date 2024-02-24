e512c312d9ee8f24ac2a6dad44be95c15c7e3bca
aditya-1404 <aditya1442002@gmail.com>
2024-02-24 14:35:19 +0530

    first

---
+++ client/src/Components/LandingPage/Animation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { lightSpeedIn } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
+
+const styles = {
+
+    lightSpeedIn: {
+        animation: 'x 10s',
+        animationName: Radium.keyframes(lightSpeedIn, 'lightSpeedIn')
+    }
+}
+
+export default class Animation extends React.Component {
+    render() {
+        return (
+            <StyleRoot>
+                <div className="animation-center" style={styles.lightSpeedIn}>
+
+                    <h1>Allow your words to escape you.</h1>
+                </div>
+            </StyleRoot>
+        )
+    }
+}
